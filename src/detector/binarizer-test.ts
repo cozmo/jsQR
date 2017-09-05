82b7b9d7b23762051e236aa2f2c1676ad89d1cc4
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2017-09-05 01:18:34 -0700

    Refactor binarizer

--- src/detector/binarizer-test.ts
+++ src/detector/binarizer-test.ts
@@ -1,7 +1,7 @@
-import { binarize } from "./binarizer";
-import * as testFixtures from "../../fixtures/fixtures.json";
-import * as png from "upng-js"
 import * as fs from "fs";
+import * as png from "upng-js";
+import * as testFixtures from "../../fixtures/fixtures.json";
+import { binarize } from "./binarizer";
 
 function loadPng(path): Promise<{width: number, height: number}> {
   return new Promise((resolve, reject) => {
@@ -15,19 +15,19 @@ function loadPng(path): Promise<{width: number, height: number}> {
 }
 
 describe("Binarize E2E", () => {
-  testFixtures.forEach((f, i) => {
-    it(`Fixture #${i}`, async () => {
+  testFixtures.forEach((f, testIndex) => {
+    it(`Fixture #${testIndex}`, async () => {
       const [inputImage, expectedOutputImage] = await Promise.all([loadPng(f.inputPath), loadPng(f.binarizedPath)]);
       const expectedOutput = png.toRGBA8(expectedOutputImage);
       const output = binarize(png.toRGBA8(inputImage), inputImage.width, inputImage.height);
-      
+
       const expectedBinaryOutput = [];
-      for (var i = 0; i < expectedOutput.length; i+=4) {
+      for (let i = 0; i < expectedOutput.length; i += 4) {
         expectedBinaryOutput.push(expectedOutput[i] === 0x00);
       }
-      const arrayMatches = output.data.length == expectedBinaryOutput.length && 
+      const arrayMatches = output.data.length === expectedBinaryOutput.length &&
         output.data.every((u, i) => (u ? 0x00 : 0xff) === expectedOutput[i * 4]);
-      expect(arrayMatches).toBeTruthy()
+      expect(arrayMatches).toBeTruthy();
     });
   });
 });
