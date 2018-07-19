cd797e63dd02b81781eb5f26ffd76ece2e81383f
David Boles <me@davidbol.es>
2018-07-18 20:40:04 -0700

    Making Travis happy round 1

--- src/color-retriever/index.ts
+++ src/color-retriever/index.ts
@@ -1,37 +1,51 @@
-import { QRLocation } from "../locator";
 import { BitMatrix } from "../BitMatrix";
+import { QRLocation } from "../locator";
+import { Point } from "../Point";
 
-//Stores two RGBA values (0-255, [r, g, b, a])
+// Stores two RGBA values (0-255, [r, g, b, a])
 export interface QRColors {
   qr: Uint8ClampedArray;
   background: Uint8ClampedArray;
 }
 
-//Retrieves the colors that make up a scanned QR code. RGB (assumed to be sRGB) values are converted to the CIELab color space for averaging (with no regard for alpha), and then converted back to RGB. Alpha values are simply averaged directly.
-export function retrieveColors(location: QRLocation, extracted: {matrix: BitMatrix; mappingFunction: Function}, sourceData: Uint8ClampedArray, sourceWidth: number): QRColors {
-  let backgroundColorTotals = [0, 0, 0, 0], qrColorTotals = [0, 0, 0, 0],//Sum totals for all the pixels as [L*, a*, b*, a].
-      backgroundPixels = 0,                 qrPixels = 0;//The number of each type of pixel that has been totaled, used to average at the end.
+/* Retrieves the colors that make up a scanned QR code. RGB (assumed to be sRGB) values are converted to the CIELab
+color space for averaging (with no regard for alpha), and then converted back to RGB. Alpha values are simply averaged
+directly. */
+export function retrieveColors(location: QRLocation, extracted: {matrix: BitMatrix;
+                               mappingFunction: (x: Number, y: Number) => Point;}, sourceData: Uint8ClampedArray,
+                               sourceWidth: number): QRColors {
+
+  // Sum totals for all the pixels as [L*, a*, b*, a].
+  let backgroundColorTotals = [0, 0, 0, 0];
+  let qrColorTotals = [0, 0, 0, 0];
+  // The number of each type of pixel that has been totaled, used to average at the end.
+  let backgroundPixels = 0;
+  let qrPixels = 0;
 
   for (let y = 0; y < location.dimension; y++) {
     for (let x = 0; x < location.dimension; x++) {
       const sourcePixel = extracted.mappingFunction(x + 0.5, y + 0.5);
       const sourcePixelOffset = ((Math.floor(sourcePixel.y) * sourceWidth) + Math.floor(sourcePixel.x)) * 4;
 
-      var sourceColor = rgbToLab(sourceData.slice(sourcePixelOffset, sourcePixelOffset + 3))
+      const sourceColor = rgbToLab(sourceData.slice(sourcePixelOffset, sourcePixelOffset + 3))
       sourceColor.push(sourceData[sourcePixelOffset + 3]);
 
       if(extracted.matrix.get(x, y)) {
-        qrColorTotals.forEach((value, componentIndex, array) => {array[componentIndex] = value + sourceColor[componentIndex]});
+        qrColorTotals.forEach((value, componentIndex, array) => {
+          array[componentIndex] = value + sourceColor[componentIndex];
+        });
         qrPixels++;
       }else {
-        backgroundColorTotals.forEach((value, componentIndex, array) => {array[componentIndex] = value + sourceColor[componentIndex]});
+        backgroundColorTotals.forEach((value, componentIndex, array) => {
+          array[componentIndex] = value + sourceColor[componentIndex];
+        });
         backgroundPixels++;
       }
     }
   }
 
-  let backgroundAverages = backgroundColorTotals.map(value => value/ backgroundPixels);
-  let qrAverages = qrColorTotals.map(value => value/ backgroundPixels);
+  const backgroundAverages = backgroundColorTotals.map(value => value/ backgroundPixels);
+  const qrAverages = qrColorTotals.map(value => value/ backgroundPixels);
 
   let backgroundColor = labToRGB(backgroundAverages);
   backgroundColor.push(backgroundAverages[3]);
@@ -40,17 +54,17 @@ export function retrieveColors(location: QRLocation, extracted: {matrix: BitMatr
 
   return {
     qr: new Uint8ClampedArray(qrColor),
-    background: new Uint8ClampedArray(backgroundColor)
+    background: new Uint8ClampedArray(backgroundColor),
   }
 }
 
 
 
-//Color space conversions from http://www.easyrgb.com/en/math.php
+// Color space conversions from http://www.easyrgb.com/en/math.php
 
-//Converts an RGB color ([r, g, b] or [r, g, b, a] - a is ignored) to CIELab ([L*, a*, b*]).
+// Converts an RGB color ([r, g, b] or [r, g, b, a] - a is ignored) to CIELab ([L*, a*, b*]).
 function rgbToLab(rgb: Uint8ClampedArray): Array<number> {
-  //To XYZ
+  // To XYZ
   var var_R = ( rgb[0] / 255 );
   var var_G = ( rgb[1] / 255 );
   var var_B = ( rgb[2] / 255 );
@@ -77,11 +91,11 @@ function rgbToLab(rgb: Uint8ClampedArray): Array<number> {
   var_G = var_G * 100;
   var_B = var_B * 100;
   
-  var X = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805;
-  var Y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722;
-  var Z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505;
+  const X = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805;
+  const Y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722;
+  const Z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505;
 
-  //To Lab
+  // To Lab
   var var_X = X / 95.047;
   var var_Y = Y / 100;
   var var_Z = Z / 108.883;
@@ -105,16 +119,16 @@ function rgbToLab(rgb: Uint8ClampedArray): Array<number> {
     var_Z = ( 7.787 * var_Z ) + ( 16 / 116 );
   }
 
-  var L = ( 116 * var_Y ) - 16;
-  var a = 500 * ( var_X - var_Y );
-  var b = 200 * ( var_Y - var_Z );
+  const L = ( 116 * var_Y ) - 16;
+  const a = 500 * ( var_X - var_Y );
+  const b = 200 * ( var_Y - var_Z );
 
   return [L, a, b];
 }
 
-//Converts a CIELab color ([L*, a*, b*] - ignores additional values) to RGB ([r, g, b]).
+// Converts a CIELab color ([L*, a*, b*] - ignores additional values) to RGB ([r, g, b]).
 function labToRGB(lab: Array<number>): Array<number> {
-  //To XYZ
+  // To XYZ
   var var_Y = ( lab[0] + 16 ) / 116;
   var var_X = lab[1] / 500 + var_Y;
   var var_Z = var_Y - lab[2] / 200;
@@ -137,11 +151,11 @@ function labToRGB(lab: Array<number>): Array<number> {
     var_Z = ( var_Z - 16 / 116 ) / 7.787;
   }
   
-  var X = var_X * 95.047;
-  var Y = var_Y * 100;
-  var Z = var_Z * 108.883;
+  const X = var_X * 95.047;
+  const Y = var_Y * 100;
+  const Z = var_Z * 108.883;
 
-  //To RGB
+  // To RGB
   var_X = X / 100;
   var_Y = Y / 100;
   var_Z = Z / 100;
@@ -168,9 +182,9 @@ function labToRGB(lab: Array<number>): Array<number> {
     var_B = 12.92 * var_B;
   }
 
-  var r = var_R * 255;
-  var g = var_G * 255;
-  var b = var_B * 255;
+  const r = var_R * 255;
+  const g = var_G * 255;
+  const b = var_B * 255;
 
   return [r, g, b];
 }
